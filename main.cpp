912763faf6db5740db6311a7261b07bd73704b58
Mostafa Gohar <mostafa-gohar@hotmail.com>
2015-12-05 00:08:19 +0200

    home coordinates

--- main.cpp
+++ main.cpp
@@ -40,7 +40,7 @@ void drawSphere();
 void handleRight();
 void handleLeft();
 double speed = 0.0003;
-double speed_multiplier=10.5;
+double speed_multiplier=1.5;
 void Display() {
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
@@ -89,7 +89,7 @@ void Display() {
 //        glutSolidSphere(0.5, 50, 50);
 //        glPopMatrix();
     drawMaze();
-    drawHome(-6,3,12);
+    drawHome(-3.52,3.5,11);
     drawSphere();
     glFlush();
 }
@@ -470,13 +470,13 @@ int main(int argc, char** argv) {
     glEnable(GL_NORMALIZE);
     glEnable(GL_COLOR_MATERIAL);
     glShadeModel(GL_SMOOTH);
-//       glMatrixMode(GL_PROJECTION);
-//    glLoadIdentity();
-//    gluPerspective(45.0f, 1000/1000, 0.1f, 300.0f);
-//    glMatrixMode(GL_MODELVIEW);
-//    glLoadIdentity();
-//    gluLookAt(30.0f, 30.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f);
-    
+   /* glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45.0f, 1000/1000, 0.1f, 300.0f);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(0.0f, 30.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, -1.0f);
+    */
     
     // Enable Lighting for this OpenGL Program
     //  glEnable(GL_LIGHTING);
