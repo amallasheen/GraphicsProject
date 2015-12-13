d7d0532961c09423761a9d0aeea6c9e47fbbf878
weammahmoud <engineer_weammahmoud@yahoo.com>
2015-12-13 14:04:23 +0200

    added music

--- main.cpp
+++ main.cpp
@@ -7,11 +7,17 @@
 //
 #include <iostream>
 #include <glut.h>
+#include <al.h>
+#include <alc.h>
+#include <alut.h>
+#include <alext.h>
+#include <windows.h>
 #include <stdlib.h>
 #include <math.h>
 #include "imageloader.h"
 using namespace std;
 #include "imageloader.cpp"
+
 void Display();
 void Anim();
 GLuint loadTexture(Image* image);
@@ -31,6 +37,16 @@ void drawBorders();
 void renderBitmapString(float x, float y, void *font, char *string);
 bool win = false;
 bool gameOver = false; int timer = 1000;
+//sndPlaySound("paradise2.wav",SND_ASYNC)
+/*using namespace irrklang;
+
+ISoundEngine *SoundEngine = createIrrKlangDevice();
+  
+/*void Init()
+{
+    SoundEngine->play2D("paradise2.wav", GL_TRUE);
+}*/
+
 void renderBitmapString(float x, float y, void *font, char *string) ///to write the time 
 {
 	char *c;
@@ -126,11 +142,13 @@ if(color_effect_check != color_effect){
 		gluPerspective(90.0f, 1000 / 1000, 0.01f, 300.0f);
 		glMatrixMode(GL_MODELVIEW);
 		glLoadIdentity();
-		gluLookAt(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, drunk_var1, 1.0, drunk_var2);
+		gluLookAt(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, 0.0, 1.0, 0.0);
+		//gluLookAt(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, drunk_var1, 1.0, drunk_var2);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		drawMaze();
 		drawHome(-3.52, 3.5, 11);
-		drawSphere();
+		drawFloor();
+		//drawSphere();
 	}
 	else if (win) {
 		//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -169,7 +187,7 @@ if(color_effect_check != color_effect){
 		glColor3f(1, 0, 0);
 		glDepthMask(0);
 		char buffer[255];
-		sprintf_s(buffer, "%s", "Game Over :Bara YA Kalb"); //%d is for integers 
+		sprintf_s(buffer, "%s", "Game Over : bara ya yad !"); //%d is for integers 
 		renderBitmapString(5.0f, 0.0f, GLUT_BITMAP_HELVETICA_18, buffer);
 
 	}
@@ -221,7 +239,7 @@ void drawMaze() {
 	//drawThirdCube();
 }
 void drawHome(double x, double y, double z) {
-	glColor3f(1, 0, 1);
+	glColor3f(1, 1, 1);
 	glPushMatrix();
 	glTranslatef(x, y, z);
 	glScalef(5, 10, 1);
@@ -238,7 +256,7 @@ void drawHome(double x, double y, double z) {
 }
 void drawAddlevel2() {
 	//left
-	glColor3f(1, 0, 0);
+	glColor3f(1, 1, 1);
 	glPushMatrix();
 	glTranslatef(-11, 0, 0);
 	glScaled(0.2, 3, 21);
@@ -327,7 +345,7 @@ void drawAddlevel2() {
 }
 void drawBorders() {
 	//left wall
-	glColor3f(1, 0, 0);
+	glColor3f(1, 1, 1);
 	glPushMatrix();
 	glTranslatef(-7, 0, 0);
 	glScaled(0.2, 3, 13);
@@ -421,7 +439,7 @@ void drawBorders() {
 }
 void drawFirstCube() {
 	//left
-	glColor3f(1, 0, 0);
+	glColor3f(1, 1, 1);
 	glPushMatrix();
 	glTranslatef(-5, 0, 0);
 	glScaled(0.2, 3, 8);
@@ -438,7 +456,7 @@ void drawFirstCube() {
 		
 	glPopMatrix();
 	//right
-	glColor3f(1, 0, 0);
+	glColor3f(1, 1, 1);
 	glPushMatrix();
 	glTranslatef(5, 0, 0);
 	glScaled(0.2, 3, 8);
@@ -455,7 +473,7 @@ void drawFirstCube() {
 		
 	glPopMatrix();
 	//top right
-	glColor3f(1, 0, 0);
+	glColor3f(1, 1, 1);
 	glPushMatrix();
 	//glRotated(90, 0, 0, 1);
 	glTranslatef(3.5, 0, -3.9);
@@ -474,7 +492,7 @@ void drawFirstCube() {
 		
 	glPopMatrix();
 	//top left
-	glColor3f(1, 0, 0);
+	glColor3f(1, 1, 1);
 	glPushMatrix();
 	//glRotated(90, 0, 0, 1);
 	glTranslatef(-3.5, 0, -3.9);
@@ -493,7 +511,7 @@ void drawFirstCube() {
 		
 	glPopMatrix();
 	//bottom
-	glColor3f(1, 0, 0);
+	glColor3f(1, 1, 1);
 	glPushMatrix();
 	//glRotated(90, 0, 0, 1);
 	glTranslatef(0, 0, 3.9);
@@ -514,7 +532,7 @@ void drawFirstCube() {
 }
 void drawSecondCube() {
 	// left up
-	glColor3f(1, 0, 0);
+	glColor3f(1, 1, 1);
 	glPushMatrix();
 	glTranslatef(-3, 0, -0.9);
 	glScaled(0.2, 3, 3);
@@ -604,7 +622,7 @@ void drawSecondCube() {
 }
 void drawThirdCube() {
 	//bottom
-	glColor3f(1, 0, 0);
+	glColor3f(1,1, 1);
 	glPushMatrix();
 	//glRotated(90, 0, 0, 1);
 	glTranslatef(0, 0, 1.5);
@@ -623,7 +641,7 @@ void drawThirdCube() {
 		
 	glPopMatrix();
 	//left
-	glColor3f(1, 0, 0);
+	glColor3f(1, 1, 1);
 	glPushMatrix();
 	glTranslatef(-1.4, 0, 0.25);
 	glScaled(0.2, 3, 2.5);
@@ -640,7 +658,7 @@ void drawThirdCube() {
 		
 	glPopMatrix();
 	//top
-	glColor3f(1, 0, 0);
+	glColor3f(1,1, 1);
 	glPushMatrix();
 	//glRotated(90, 0, 0, 1);
 	glTranslatef(0, 0, -1);
@@ -676,7 +694,7 @@ void drawThirdCube() {
 	glPopMatrix();
 }
 void drawAddlevel1() {
-	glColor3f(1, 0, 0);
+	glColor3f(1, 1, 1);
 	//left
 	glPushMatrix();
 	glTranslatef(-9, 0, 0);
@@ -710,7 +728,7 @@ void drawAddlevel1() {
 		
 	glPopMatrix();
 	//top right
-	glColor3f(1, 0, 0);
+	glColor3f(1, 1,1);
 	glPushMatrix();
 	//glRotated(90, 0, 0, 1);
 	glTranslatef(7.5, 0, -8.4);
@@ -729,7 +747,7 @@ void drawAddlevel1() {
 		
 	glPopMatrix();
 	//top left
-	glColor3f(1, 0, 0);
+	glColor3f(1, 1, 1);
 	glPushMatrix();
 	//glRotated(90, 0, 0, 1);
 	glTranslatef(-4.1, 0, -8.5);
@@ -883,7 +901,7 @@ void Anim() {
 		//glTranslatef(eyeX + ((centerX - eyeX) / 30.0), -1.0, eyeZ + ((centerZ - eyeZ) / 30.0));
 		if (eyeX < 3.5 && eyeX > -3 && eyeZ <= 3 && eyeZ >= -2.5){//asgher cube
 			if (eyeX < -2.2 && eyeZ<1.7&&eyeZ>0.7){// el fahaa
-				printf_s("%s", "   now i am here  ");
+				//printf_s("%s", "   now i am here  ");
 				glutPostRedisplay();
 				return;
 			}
@@ -905,7 +923,7 @@ void Anim() {
 			if (eyeZ < -3.1){//up //hina feh fathaa //it was 3.8 then 3.3 
 				if (eyeX >= -1.6&&eyeX <= 2.2){
 					//hasibo y3ady el fatha
-					printf_s("%s", "   ana fel fathaa  ");   ///--->de
+					//printf_s("%s", "   ana fel fathaa  ");   ///--->de
 				}
 				else {
 					eyeZ += 1.2;
@@ -925,7 +943,7 @@ void Anim() {
 				eyeX -= 0.7;
 			}
 			else if (eyeX <3.8 && eyeX > -3.3 && eyeZ<3.9&&eyeZ>2.6){//kan tht wi hab ytl3 --->d
-				printf_s("%s", "   hhh  ");
+				//printf_s("%s", "   hhh  ");
 				if (eyeZ <= 3){ //it was 3 
 
 					eyeZ += 0.5;
@@ -940,9 +958,9 @@ void Anim() {
 
 			}
 			else if (eyeX > 3.2&&eyeZ > -2.5&&eyeZ < 3){//mashy shmal wi hykhbt fel soghnn eyex>3.2 -->d
-				printf_s("%s", " yes yes ");
+				//printf_s("%s", " yes yes ");
 				if (eyeX<3.8){//it was 3.4
-					printf_s("%s", "ana hinaa");
+					//printf_s("%s", "ana hinaa");
 					eyeX += 1.2;
 				}
 
@@ -982,7 +1000,7 @@ void Anim() {
 				eyeX -= 1;
 			}
 			else if (eyeX <5.6 && eyeX > -5.3 && eyeZ<7 && eyeZ>3.9){//kan tht wi hab ytl3s fmmkn ykhbt fel soghnn  -->d
-				printf_s("%s", "   cocococo  ");
+				//printf_s("%s", "   cocococo  ");
 				if (eyeZ < 5){ //it was 4.59
 
 					eyeZ += 1;
@@ -1033,7 +1051,7 @@ void Anim() {
 				eyeX -= 0.09;
 			}
 			else if (eyeX <8.3 && eyeX > -7.5 && eyeZ<8.5 && eyeZ>6.5){//kan tht wi hab ytl3s fmmkn ykhbt fel soghnn
-				printf_s("%s", "   cocococo  ");//still lazm akhod baly mn el fatha
+				//printf_s("%s", "   cocococo  ");//still lazm akhod baly mn el fatha
 
 				if (eyeZ < 7.55){ //it was 7.59   ---->d
 					//eyeX = eyeX;
@@ -1087,7 +1105,7 @@ void Anim() {
 				eyeX -= 0.2;
 			}
 			else if (eyeX <10.4 && eyeX > -9.5 && eyeZ<10.5 && eyeZ>8.5){//kan tht wi hab ytl3s fmmkn ykhbt fel soghnneyex>-10 /eyex<10.5/
-				printf_s("%s", "   cocococo  ");//still lazm akhod baly mn el fatha ---->d
+				//printf_s("%s", "   cocococo  ");//still lazm akhod baly mn el fatha ---->d
 				if (eyeZ < 9.7){
 
 					eyeZ += 0.7;
@@ -1133,7 +1151,7 @@ int main(int argc, char** argv) {
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 	glutKeyboardFunc(Key);
 	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_LIGHTING);
+	//glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
 	glEnable(GL_NORMALIZE);
 	glEnable(GL_COLOR_MATERIAL);
@@ -1142,8 +1160,14 @@ int main(int argc, char** argv) {
 	_textureId = loadTexture(image1);
 	Image* image5 = loadBMP("textures/door.bmp");
 	_textureId2 = loadTexture(image5);
-	Image* image6= loadBMP("textures/jupiter.bmp");
+	Image* image6= loadBMP("textures/pattern9.bmp");
 	_textureId3= loadTexture(image6);
+    
+	PlaySoundA("paradise2.wav",NULL,SND_ASYNC);
+
+	//alutInit(0, NULL);
+	//Init();
+	//play2D("paradise2.wav"); 
 
 	/* glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
